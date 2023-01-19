4b7486936074d6c61fd00617dd3365195b0e0b04
William Wang <wmwang52@gmail.com>
2023-01-19 09:59:15 -0500

    Final

--- A1Main.java
+++ A1Main.java
@@ -5,25 +5,27 @@ import java.util.Scanner;  // Import the Scanner class
 public class A1Main {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
+
         //Some notes and hints for Task 2:
         /* Declare and create two objects of type Summer and for
          the parameter to the constructor use 30...  meaning each
          Summer object can deal with up to 30 ints sent to it */
-     
-        /*Your code here */
 
+        /*Your code here */
+        Summer oddNums = new Summer(30);
+        Summer evenNums = new Summer(30);
         /* Now get a single integer from the scanner
          lets refer to that int as N
          we will use N as an upper limit in a for loop */
 
         /*Your code here */
-        
+        int Userinput = input.nextInt();
         // Now loop N times.
         /* In each loop iteration do these things:
            -- get an integer from the input scanner
            -- decide if that int is even or odd
-           -- if its even , send it to the even Summer obj
+
               by calling the "add" method
            -- if that int is odd, send it to the odd Summer obj
               by calling its "add" method
@@ -34,64 +36,73 @@ public class A1Main {
         // one Summer obj is full of even ints
         // the other Summer obj is full of odd ints
 
-         /*Your code here */
-        
+        /*Your code here */
+        for(var i = 0; i < Userinput; i ++){
+            int userNumber = input.nextInt();
+            if (userNumber % 2 == 0){
+                evenNums.add(userNumber);
+            } else if(userNumber % 2 != 0) {
+                oddNums.add(userNumber);
+            }
+        }
+
         /* Now do the requested output as shown in the samples
          and we are done.  Hint:  This is easier with the printStats method. */
 
         /*Your code here */
-       
-  } 
+        printStats(evenNums, oddNums);
+    }
 
-      
-  static void printStats (Summer evenSummer, Summer oddSummer) {
-      //This is a helper method you can use for Task 2. 
+
+    static void printStats (Summer evenSummer, Summer oddSummer) {
+        //This is a helper method you can use for Task 2.
       /*Hint: Pay careful attention to whether or not your
       inserted code prints output on the same line or a new line. */
-    
-    
-      System.out.print("Number of evens: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Sum of evens: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Average of evens: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Largest even: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Smallest even: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Average of evens over 10: ");
-      /*Your code here */
-      System.out.println();
-      
-      System.out.print("Number of odds: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Sum of odds: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Average of odds: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Largest odd: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Smallest odd: ");
-      /*Your code here */
-      System.out.println();
-      System.out.print("Average of odds over 10: ");
-      /*Your code here */
-      System.out.println();
-  }
 
 
-  // Hint: For task 2, you may wish to write a static method "even" here that
-  // would return a boolean when sent an int n.
-  // Do that if you like, or put your even test inline in main.
+        System.out.print("Number of evens: ");
+        System.out.print(evenSummer.count());
+        System.out.println();
+        System.out.print("Sum of evens: ");
+        System.out.print(evenSummer.sum());
+        System.out.println();
+        System.out.print("Average of evens: ");
+        System.out.print(evenSummer.average());
+        System.out.println();
+        System.out.print("Largest even: ");
+        System.out.print(evenSummer.high());
+        System.out.println();
+        System.out.print("Smallest even: ");
+        System.out.print(evenSummer.low());
+        System.out.println();
+        System.out.print("Average of evens over 10: ");
+        System.out.print(evenSummer.avgOver(10));
+        System.out.println();
+
+        System.out.print("Number of odds: ");
+        System.out.print(oddSummer.count());
+        System.out.println();
+        System.out.print("Sum of odds: ");
+        System.out.print(oddSummer.sum());
+        System.out.println();
+        System.out.print("Average of odds: ");
+        System.out.print(oddSummer.average());
+        System.out.println();
+        System.out.print("Largest odd: ");
+        System.out.print(oddSummer.high());
+        System.out.println();
+        System.out.print("Smallest odd: ");
+        System.out.print(oddSummer.low());
+        System.out.println();
+        System.out.print("Average of odds over 10: ");
+        System.out.print(oddSummer.avgOver(10));
+
+        System.out.println();
+    }
+
+
+    // Hint: For task 2, you may wish to write a static method "even" here that
+    // would return a boolean when sent an int n.
+    // Do that if you like, or put your even test inline in main.
 
 } 
--- Summer.java
+++ Summer.java
@@ -11,6 +11,7 @@ package a1;
 public class Summer {
    private int[] nums;
    // Any other data fields you decide you need can go here.
+   private int openSlot = 0;
 
    public Summer(int n) {
       /* In this constructor, first allocate an array that hold n ints and put that in nums.
@@ -18,62 +19,99 @@ public class Summer {
       you would need to do something with that field here.
       */
       /*Your code here */
+      int[] hello = new int[n];
+      this.nums = hello;
+
    }
 
    public void add( int num ) {
-     // In this method, take the parameter num and put that value into the Summer.
+      // In this method, take the parameter num and put that value into the Summer.
       /*Your code here */
+      nums[openSlot] = num;
+      if (openSlot < nums.length){
+         openSlot++;
+      } else{
+         System.out.println("There are no more open spaces");
+      }
    }
-   
-   public int sum ( ) { 
+
+   public int sum ( ) {
      /* In this method, go through the array and add up all the ints
       that have been stored in it so far.
       Note that the sum of ints is of type int, so return the int sum you get.  */
-
+      int temp = 0;
       /*Your code here*/
+      for(int i: nums){
+         temp = temp + i;
+      }
+      return temp;
    }
-   
-   public int high ( ) { 
+
+   public int high ( ) {
      /* Find the largest number stored in the array so far
       and return it. */
-
+      int start = nums[0];
       /*Your code here */
+      for(int i = 1; i < openSlot; i++){
+         if (start < nums[i]){
+            start = nums[i];
+         }
+      }
+      return start;
    }
-   
-   public int low ( ) { 
+
+   public int low ( ) {
      /* Find the smallest number stored in the array so far
       and return it. */
-
+      int start = nums[0];
+      /*Your code here */
+      for(int i = 1; i < openSlot; i++){
+         if (start > nums[i]){
+            start = nums[i];
+         }
+      }
+      return start;
       /*Your code here */
    }
-   
+
    public double average( ) {
      /* Find the average of all the numbers stored in the array
       so far, and return it.
       Note that the average of ints might produce a double, so
       we are returning a double.
-     
+
       HEADS UP
-      This method might get called before there are any 
+      This method might get called before there are any
       ints stored in the array, so you need to guard
       against divide by zero.
       We will simply define the average of no numbers
       to be 0.0 so return that if the array has no numbers in it.
        */
-
       /*Your code here */
+      if (openSlot > 0) {
+         double total = 0.0;
 
+         for (int i : nums) {
+            total = total + i;
+         }
+         if (total != 0.0) {
+            return total / (double) openSlot;
+         } else {
+            return 0.0;
+         }
+
+      }
+      return 0.0;
    }
-   
    public double avgOver( int thresh ) {
 
    /* Similar to average() except we want to only average up
       stored numbers that are ABOVE ( > ) the threshold
       parameter "thresh".
-      
-      Hint: Look at examples 1 and 2 in the readme make sure you 
+
+      Hint: Look at examples 1 and 2 in the readme make sure you
       understand how to calculate avgOver.
-     
+
       HEADS UP
       We have the same divide by zero issue here as in average()
       so again if you are trying to take an average on no numbers
@@ -81,12 +119,27 @@ public class Summer {
    */
 
       /*Your code here */
+      if (openSlot > 0) {
+         float total = 0;
+         float num = 0;
+         for (int i : nums) {
+            if (i > thresh) {
+               total = total + i;
+               num++;
+            }
+         }
+         if (total != 0.0) {
+            return total / num;
+         } else {
+            return 0.0;
+         }
+      }
+      return 0.0;
    }
-   
-   public int count ( ) {  
-     //Return the number of ints that have been stored in the array.
-       
-     /*Your code here */
+   public int count ( ) {
+      //Return the number of ints that have been stored in the array.
 
+      /*Your code here */
+      return openSlot;
    }
 }
